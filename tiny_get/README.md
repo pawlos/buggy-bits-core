c56462184db8fb5c08d8ec55588f41be50c97d09
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2017-09-24 12:04:43 +0200

    Added TinyGet tool

---
+++ tiny_get/README.md
@@ -0,0 +1,9 @@
+TinyGet
+=======
+
+TinyGet is a very simple console app for making GET requests.
+Original idea belongs to TinyGet from IIS ResourceKit 6 http://www.microsoft.com/en-us/download/details.aspx?id=17275.
+
+Download
+--------
+Latest build - [here](https://api.solidbuild.net/v1/1/scripts/1052/builds/last-successful/artifacts/TinyGet.zip).
---
+++ tiny_get/TinyGet/Requests/RequestSender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TinyGet.Requests
+{
+    internal class RequestSender : IRequestSender
+    {
+        private readonly Context _context;
+
+        public RequestSender(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task Run()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                for (int i = 0; i < _context.Arguments.Loop; i++)
+                {
+                    HttpRequestMessage request = new HttpRequestMessage(_context.Arguments.Method, _context.Arguments.GetUrl());
+                    HttpResponseMessage result = await client.SendAsync(request, _context.Token);
+                    if ((int)result.StatusCode != _context.Arguments.Status)
+                    {
+                        throw new ApplicationException("Status code is not equal to " + _context.Arguments.Status);
+                    }
+                }
+            }
+        }
+    }
+}
